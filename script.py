1b92e1753f994a65709e45fc13ab777ec303e265
Oskar Przybylski <oskar.przybylski23@gmail.com>
2025-04-14 16:50:40 +0100

    create a GH action

--- script.py
+++ script.py
@@ -4,8 +4,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file(
-    "credentials.json", scopes=scopes)
+creds = Credentials.from_service_account_info()
 client = gspread.authorize(creds)
 
 sheet_id = "1t7iOjmWEitEz6_E1g-YqqB7UiWM5OzVvmlAJCQQqiEY"
